294d21d85acda9a5ef10384877e9ed5378675d08
Darius <1357280829@qq.com>
2020-09-10 22:28:31 +0800

    微信用户加入软删除

--- app/Models/WechatUser.php
+++ app/Models/WechatUser.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class WechatUser extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'nickname', 'phone', 'unionid', 'openid_mini_program', 'openid_official_account', 'avatar_url', 'gender',
         'country', 'province', 'city',
